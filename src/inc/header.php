61b9c6a6f32fca9d242823021fd28b9a48d189fb
Benjamin Hollway <benjamin@nothingrandom.com>
2015-10-17 10:56:51 +0100

    Replace with new barebones

---
+++ src/inc/header.php
@@ -0,0 +1,17 @@
+<header class="header main-header">
+	<div class="row">
+		<div class="column small-12 medium-6">
+			<div class="logo">
+				<p><a href="http://nothingrandom.com">bh.</a></p>
+			</div>
+		</div>
+		<div class="column small-12 medium-6">
+			<nav class="main-nav">
+				<ul>
+					<li><a href="http://nothingrandom.com">Work</a></li>
+					<li><a href="http://nothingrandom.com/is">Profile</a></li>
+					<li><a href="http://nothingrandom.com/thinks">Journal</a></li>
+				</ul>
+		</div>
+	</div>
+</header>
