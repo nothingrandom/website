35d74bf5b15bec41568ddb7fa8ec3c4d55ac7b41
Benjamin Hollway <benjamin@nothingrandom.com>
2016-09-14 15:23:21 +0100

    Create new branch

--- src/inc/footer.php
+++
@@ -1,8 +0,0 @@
-	<footer class="main-footer">
-		<p>&copy; Benjamin Hollway | <span id="copyyear">2014</span></p>
-		<div class="page-line page-line--short page-line--mid">&nbsp;</div>
-		<p class="text--italic"><a href="mailto:b@nothingrandom.com">b@nothingrandom.com</a></p>
-	</footer>
-	<script src="/assets/js/vendor/retina.js" type="text/javascript"></script>
-	</body>
-</html>
