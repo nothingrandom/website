674ba9758acdbf6071e2960dc6fb7cb9bd913422
Benjamin Hollway <benjamin@nothingrandom.com>
2016-10-26 23:28:28 +0100

    Markup. Primary styles

---
+++ src/index.php
@@ -0,0 +1,174 @@
+<?php include('inc/head.php') ?>
+<?php include('inc/header.php') ?>
+
+<main class="page-wrapper page-wrapper--index">
+	<div class="background-image"></div>
+	<div class="page-section section--hero">
+		<div class="row">
+			<div class="column small-12 medium-9 large-7">
+				<h1>Benjamin Hollway is a front end developer.</h1>
+				<h2>Currently based in Brighton, UK and creating web experiences for 4 years. Specialising in animation, simplicity, user interaction and design. <a href="">+</a></h2>
+				<ul class="social-links">
+					<li><a href="">Twitter</a></li>
+					<li><a href="">Instagram</a></li>
+					<li><a href="">GitHub</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<div class="page-section section--blog section--highlight">
+		<h3>Writing</h3> <!-- large text, sits behind, faint. parallax -->
+		<div class="row">
+			<div class="column medium-6">
+				<h4>Latest post</h4>
+				<h5><a href="">Inaugural</a></h5>
+				<p>It’s time I started to put pen to paper regularly <a href="">+</a></p>
+			</div>
+			<div class="column medium-6">
+				<h4>Older posts</h4>
+				<ul>
+					<li><a href="">Lorem ipsum dolor sit amet.</a></li>
+					<li><a href="">Lorem ipsum dolor sit amet.</a></li>
+					<li><a href="">Lorem ipsum dolor sit amet.</a></li>
+					<li><a href="">See more.</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<div class="page-section section--projects">
+		<h3>Projects</h3>
+		<div class="row">
+			<div class="column small-12">
+				<h2>A collection of my latest work and creations &mdash;</h2>
+			</div>
+			<div class="column large-6">
+				<div class="project__item">
+					<img src="http://placehold.it/650x500" />
+					<div class="project__info">
+						<h5>Project title</h5>
+						<h4>development</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut officiis quod consequatur asperiores, possimus quia.</p>
+					</div>
+				</div>
+			</div>
+			<div class="column large-6">
+				<div class="project__item">
+					<img src="http://placehold.it/650x500" />
+					<div class="project__info">
+						<h5>Project title</h5>
+						<h4>development</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut officiis quod consequatur asperiores, possimus quia.</p>
+					</div>
+				</div>
+			</div>
+			<div class="column large-8">
+				<div class="project__item">
+					<img src="http://placehold.it/878x500" />
+					<div class="project__info">
+						<h5>Project title</h5>
+						<h4>development</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut officiis quod consequatur asperiores, possimus quia.</p>
+					</div>
+				</div>
+			</div>
+			<div class="column large-4">
+				<div class="project__item">
+					<img src="http://placehold.it/423x235" />
+					<div class="project__info">
+						<h5>Project title</h5>
+						<h4>development</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut officiis quod consequatur asperiores, possimus quia.</p>
+					</div>
+				</div>
+				<div class="project__item">
+					<img src="http://placehold.it/423x235" />
+					<div class="project__info">
+						<h5>Project title</h5>
+						<h4>development</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut officiis quod consequatur asperiores, possimus quia.</p>
+					</div>
+				</div>
+			</div>
+			<div class="column large-12">
+				<div class="project__item">
+					<img src="http://placehold.it/1333x500" />
+					<div class="project__info">
+						<h5>Project title</h5>
+						<h4>development</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut officiis quod consequatur asperiores, possimus quia.</p>
+					</div>
+				</div>
+			</div>
+			<div class="column large-4">
+				<div class="project__item">
+					<img src="http://placehold.it/423x235" />
+					<div class="project__info">
+						<h5>Project title</h5>
+						<h4>development</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut officiis quod consequatur asperiores, possimus quia.</p>
+					</div>
+				</div>
+				<div class="project__item">
+					<img src="http://placehold.it/423x235" />
+					<div class="project__info">
+						<h5>Project title</h5>
+						<h4>development</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut officiis quod consequatur asperiores, possimus quia.</p>
+					</div>
+				</div>
+			</div>
+			<div class="column large-8">
+				<div class="project__item">
+					<img src="http://placehold.it/878x500" />
+					<div class="project__info">
+						<h5>Project title</h5>
+						<h4>development</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut officiis quod consequatur asperiores, possimus quia.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="page-section section--photos section--highlight">
+		<h2>Sometimes I go outside &mdash;</h2>
+		<h3>Photographs</h3>
+		<ul class="social-links">
+			<li><a href="">Instagram</a></li>
+			<li><a href="">VSCO</a></li>
+		</ul>
+		<p>Photos currently taken with a Nikon D5300 or OnePlus Three</p>
+	</div>
+	<div class="page-section section--contact">
+		<h3>Contact</h3>
+		<div class="row">
+			<div class="column large-6">
+				<p>Currently available for freelance and contract projects.</p>
+				WNW
+			</div>
+			<div class="column large-6">
+				<a href="" class="btn btn--large btn--dark">b [at] nothingrandom.com</a>
+				<p><a href="">+44 1273 931397</a></p>
+			</div>
+			<div class="column medium-10 large-8 medium-centered">
+				<ul class="social-links social-links--icons">
+					<!-- social icons -->
+					<li><a href="">Instagram</a></li>
+					<li><a href="">Twitter</a></li>
+					<li><a href="">Medium</a></li>
+					<li><a href="">Dribbble</a></li>
+					<li><a href="">Github</a></li>
+					<li><a href="">Strava</a></li>
+				</ul>
+			</div>
+		</div>
+
+		<!--
+			social links
+			currently available for freelance
+			contact form?
+		-->
+	</div>
+</main>
+
+<?php include('inc/footer.php') ?>
