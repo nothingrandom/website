f235feb5caf7ddb6675e73ebbb9449240697dccb
Benjamin Hollway <benjamin@nothingrandom.com>
2017-09-17 22:48:37 +0200

    Add smooth scroll

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -8,8 +8,8 @@ window.console.log.apply(console, e);
 $(function() {
   var $content = $('#js-blog'),
     data = {
-    rss_url: 'https://medium.com/feed/@nothingrandom'
-  };
+      rss_url: 'https://medium.com/feed/@nothingrandom'
+    };
 
   $.get('https://api.rss2json.com/v1/api.json', data, function(response) {
     if (response.status == 'ok') {
@@ -35,4 +35,39 @@ $(function() {
       $content.html(output);
     }
   });
+});
+
+// Select all links with hashes
+$('a[href*="#"]')
+// Remove links that don't actually link to anything
+  .not('[href="#"]').not('[href="#0"]').click(function(event) {
+  // On-page links
+  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    // Figure out element to scroll to
+    var target = $(this.hash);
+    target = target.length
+      ? target
+      : $('[name=' + this.hash.slice(1) + ']');
+    // Does a scroll target exist?
+    if (target.length) {
+      // Only prevent default if animation is actually gonna happen
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000, function() {
+        // Callback after animation
+        // Must change focus!
+        var $target = $(target);
+        $target.focus();
+        if ($target.is(':focus')) { // Checking if the target was focused
+          return false;
+        }
+
+        else {
+          $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+          $target.focus(); // Set focus again
+        }
+      });
+    }
+  }
 });
