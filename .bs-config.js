b85462d0d6e496aac9a6345fcba85b521099a033
Benjamin Grant <benjamin@nothingrandom.com>
2019-04-30 16:59:07 +0200

    Fix browser-sync and caching stuff

--- .bs-config.js
+++ .bs-config.js
@@ -2,13 +2,13 @@
 
 /**
  * The browser-sync configuration.
- * 
+ *
  * For a full list of options, see: http://www.browsersync.io/docs/options/
  */
 module.exports = {
     serveStatic: ['./dist'],
     serveStaticOptions: {
-        extensions: ['html']
+        extensions: ['html', 'js']
     },
     https: false,
     open: false,
