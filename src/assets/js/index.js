2c67efe504ab807b52a59431e353950db4ef9ede
Benjamin Hollway <benjamin@nothingrandom.com>
2019-05-03 09:02:25 +0200

    Change font-awesome from cdn to yarn

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,1 +1,9 @@
 // console.log('Hello world from /js/index.js!');
+import { library, dom } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { fal } from '@fortawesome/pro-light-svg-icons';
+
+library.add(fab);
+library.add(fal);
+
+dom.watch();
