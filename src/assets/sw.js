ac3dfae858b9c5af0f7595bd11d3357d56aa9f1b
Benjamin Grant <benjamin@nothingrandom.com>
2019-05-20 10:39:07 +0200

    Update service worker

--- src/assets/sw.js
+++ src/assets/sw.js
@@ -9,26 +9,59 @@ importScripts('/workbox/workbox-v4.2.0/workbox-sw.js');
 // Then, we add placeholder for the precache routes manifest
 // DO NOT REMOVE! :)
 
-const cacheImages = () => {
-  workbox.routing.registerRoute(
-    /\.(?:png|gif|jpg|jpeg|webp|svg)$/,
-    new workbox.strategies.CacheFirst({
-      cacheName: 'images',
-      plugins: [
-        new workbox.expiration.Plugin({
-          maxEntries: 60,
-          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-        }),
-      ],
-    }),
-  );
-};
-
-if (process.env.NODE_ENV === 'production') {
-  workbox.precaching.precacheAndRoute([]);
-} else {
-  cacheImages();
-  // workbox.precaching.precacheAndRoute([]);
-}
+// workbox.precaching.precacheAndRoute([]);
 
 // Put any of your custom workbox logic below!
+
+workbox.routing.registerRoute(
+  /\.(?:png|gif|jpg|jpeg|webp|svg)$/,
+  new workbox.strategies.CacheFirst({
+    cacheName: 'images',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      }),
+    ],
+  }),
+);
+
+workbox.routing.registerRoute(
+  /\.(?:js|css)$/,
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'static-resources',
+  }),
+);
+
+workbox.routing.registerRoute(
+  /\.(?:html)$/,
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'static-pages',
+  }),
+);
+
+// Cache the Google Fonts stylesheets with a stale-while-revalidate strategy.
+workbox.routing.registerRoute(
+  /^https:\/\/fonts\.googleapis\.com/,
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'google-fonts-stylesheets',
+  })
+);
+
+// Cache the underlying font files with a cache-first strategy for 1 year.
+workbox.routing.registerRoute(
+  /^https:\/\/fonts\.gstatic\.com/,
+  new workbox.strategies.CacheFirst({
+    cacheName: 'google-fonts-webfonts',
+    plugins: [
+      new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200],
+      }),
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 60 * 60 * 24 * 365,
+        maxEntries: 30,
+      }),
+    ],
+  })
+);
+
