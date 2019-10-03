e0d6b6a542d2cf88cbc3fa7f7126e4cea95db392
Benjamin Grant <benjamin@nothingrandom.com>
2019-10-03 14:13:03 +0200

    Add linter

---
+++ eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint',
+    sourceType: 'module',
+  },
+  env: {
+    browser: true,
+    es6: true,
+  },
+  globals: {
+  },
+  extends: ['eslint:recommended', 'airbnb-base'],
+  plugins: [
+  ],
+  rules: {
+    'max-len': ['error', 120, 2, {
+      ignoreUrls: true,
+      ignoreComments: false,
+      ignoreRegExpLiterals: true,
+      ignoreStrings: true,
+      ignoreTemplateLiterals: true,
+    }],
+  },
+};
