886be8599fbd08c30c8ac06f612d2bdf9c0e0149
Benjamin Hollway <benjamin@nothingrandom.com>
2020-01-28 15:48:19 +0100

    Fix build

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,3 @@
-// module.exports = function(eleventyConfig) {
-//   eleventyConfig.addPassthroughCopy('src/images')
-
-//   return {
-//     dir: { input: 'src', output: 'dist', data: '_data' },
-//     passthroughFileCopy: true,
-//     templateFormats: ['njk', 'md', 'css', 'html', 'yml'],
-//     htmlTemplateEngine: 'njk'
-//   }
-// }
-
 "use strict";
 
 const glob = require('fast-glob');
