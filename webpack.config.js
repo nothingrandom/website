9fc6038c012422e402a0861eaba03fd169aef911
Benjamin Hollway <benjamin@nothingrandom.com>
2020-01-28 14:55:25 +0100

    Move whole site away from parceleventy boilerplate as experiment

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist/scripts'),
+    filename: 'index.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+}
