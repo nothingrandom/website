320402bed05c925485b09a4f1ef3789a76df6830
Benjamin Grant <benjamin@nothingrandom.com>
2020-02-24 23:12:43 +0100

    Add search

--- utils/filters.js
+++ utils/filters.js
@@ -1,5 +1,11 @@
 const { DateTime } = require('luxon');
 
+const removeHTML = (text) => {
+  // remove all html elements and new lines
+  const content = new String(text);
+  return unescape(content.replace(/(&lt;.*?&gt;)|(<.*?>)/gi, ' '));
+};
+
 module.exports = {
   readableDate(date) {
     return DateTime.fromJSDate(date).toFormat('dd LLLL yyyy');
@@ -40,5 +46,30 @@ module.exports = {
     }
 
     return false;
+  },
+  removeHTML,
+  squash(text) {
+    let content = new String(text);
+
+    // all lower case, please
+    content = content.toLowerCase();
+
+    // remove all html elements and new lines
+    // const plain = unescape(content.replace(/(&lt;.*?&gt;)|(<.*?>)/gi, ' '));
+    const plain = removeHTML(content);
+
+    // remove duplicated words
+    const words = plain.split(' ');
+    const deduped = [...(new Set(words))];
+    const dedupedStr = deduped.join(' ')
+
+    // remove short and less meaningful words
+    let result = dedupedStr.replace(/\b(\.|\,|the|a|an|and|am|you|I|to|if|of|off|me|my|on|in|it|is|at|as|we|do|be|has|but|was|so|no|not|or|up|for)\b/gi, '');
+    // remove newlines, and punctuation
+    result = result.replace(/\.|\,|\?|-|—|\n/g, '');
+    // remove repeated spaces
+    result = result.replace(/[ ]{2,}/g, ' ');
+
+    return result;
   },
 };
