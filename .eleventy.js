684aec7748ccd239ce6970f25de6bc132284d007
Benjamin Hollway <benjamin@nothingrandom.com>
2020-02-28 10:35:11 +0100

    Add postbuild check to travis to validate/lint output of json

--- .eleventy.js
+++ .eleventy.js
@@ -8,47 +8,47 @@ const readingTime = require('eleventy-plugin-reading-time');
 // For a full list of options, see: https://www.11ty.io/docs/config/
 
 module.exports = (eleventyConfig) => {
-    const dirs = {
-        input: 'src',
-        data: '_data',
-        includes: '_includes',
-        output: 'dist',
-    }
+  const dirs = {
+    input: 'src',
+    data: '_data',
+    includes: '_includes',
+    output: 'dist',
+  }
 
-    // Filters
-    Object.keys(filters).forEach(filterName => {
-        eleventyConfig.addFilter(filterName, filters[filterName])
-    })
+  // Filters
+  Object.keys(filters).forEach(filterName => {
+    eleventyConfig.addFilter(filterName, filters[filterName])
+  })
 
-    // Shortcodes
-    Object.keys(shortcodes).forEach(shortCodeName => {
-        eleventyConfig.addShortcode(shortCodeName, shortcodes[shortCodeName])
-    })
+  // Shortcodes
+  Object.keys(shortcodes).forEach(shortCodeName => {
+    eleventyConfig.addShortcode(shortCodeName, shortcodes[shortCodeName])
+  })
 
-    eleventyConfig.addPassthroughCopy('_redirects');
-    eleventyConfig.addPassthroughCopy('src/img');
-    eleventyConfig.addPassthroughCopy('src/fonts');
+  eleventyConfig.addPassthroughCopy('_redirects');
+  eleventyConfig.addPassthroughCopy('src/img');
+  eleventyConfig.addPassthroughCopy('src/fonts');
 
-    eleventyConfig.addPlugin(readingTime);
+  eleventyConfig.addPlugin(readingTime);
 
-    return {
-        // Set the path from the root of the deploy domain
-        // i.e, example.com + "/"
-        pathPrefix: "/",
+  return {
+    // Set the path from the root of the deploy domain
+    // i.e, example.com + "/"
+    pathPrefix: "/",
 
-        // Set the src and output directories
-        dir: dirs,
+    // Set the src and output directories
+    dir: dirs,
 
-        // Set the default template engine from `liquid` to `njk`
-        htmlTemplateEngine: "njk",
-        markdownTemplateEngine: "njk",
-        dataTemplateEngine: "njk",
+    // Set the default template engine from `liquid` to `njk`
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
 
-        // Set up eleventy to pass-through files to be compiled by Parcel
-        passthroughFileCopy: true
-    };
+    // Set up eleventy to pass-through files to be compiled by Parcel
+    passthroughFileCopy: true
+  };
 };
 
 function resolveNameFromPath(path) {
-    return path.basename(path);
+  return path.basename(path);
 }
