890d17064b0649ad776755e39ef420a40499ac05
Benjamin Hollway <benjamin@nothingrandom.com>
2020-02-28 12:36:58 +0100

    Make search better

---
+++ src/_data/search-regex.js
@@ -0,0 +1,4 @@
+// ENV variables
+module.exports = {
+  meaningless: /\b(\.|\,|the|a|an|[A-Za-z]+('[A-Za-z]+)|and|am|you|I|to|if|of|off|me|my|on|in|it|is|at|as|we|do|be|has|but|was|so|no|not|or|up|for)\b/gi,
+};
