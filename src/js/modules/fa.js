884c24797b8c9bbf96f058f70b2d6c145d395d75
Benjamin Grant <benjamin@nothingrandom.com>
2022-01-28 15:10:59 +0000

    feat: upgrade to font-awesome 6 beta

--- src/js/modules/fa.js
+++ src/js/modules/fa.js
@@ -1,39 +1,42 @@
 // console.log('Hello world from /js/index.js!');
 import { library, dom } from '@fortawesome/fontawesome-svg-core';
 
-import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
-import { faInstagram } from '@fortawesome/free-brands-svg-icons/faInstagram';
-import { faTwitter } from '@fortawesome/free-brands-svg-icons/faTwitter';
+import {
+  faGithub,
+  faInstagram,
+  faTwitter,
+} from '@fortawesome/free-brands-svg-icons';
 
-import { faSearch } from '@fortawesome/pro-regular-svg-icons/faSearch';
+import {
+  faMusic,
+  faSearch,
+  faBurgerCheese,
+  faXmark,
+  faEyes,
+  faCode,
+  faHandsHoldingHeart,
+  faCoffeePot,
+  faLongArrowLeft,
+  faForkKnife as fatForkKnife,
+} from '@fortawesome/pro-thin-svg-icons';
 
-import { faCoffeeTogo } from '@fortawesome/pro-duotone-svg-icons/faCoffeeTogo';
-import { faHandHoldingHeart } from '@fortawesome/pro-duotone-svg-icons/faHandHoldingHeart';
-import { faUtensilsAlt as fadUtensilsAlt } from '@fortawesome/pro-duotone-svg-icons/faUtensilsAlt';
-
-import { faCheeseburger } from '@fortawesome/pro-light-svg-icons/faCheeseburger';
-import { faCode } from '@fortawesome/pro-light-svg-icons/faCode';
-import { faEye } from '@fortawesome/pro-light-svg-icons/faEye';
-import { faUtensilsAlt as falUtensilsAlt } from '@fortawesome/pro-light-svg-icons/faUtensilsAlt';
-import { faLongArrowLeft } from '@fortawesome/pro-light-svg-icons/faLongArrowLeft';
-import { faMusic } from '@fortawesome/pro-solid-svg-icons/faMusic';
-import { faTimes } from '@fortawesome/pro-light-svg-icons/faTimes';
+import { faForkKnife as fadForkKnife } from '@fortawesome/pro-duotone-svg-icons';
 
 library.add(
   faGithub,
   faInstagram,
   faTwitter,
-  faSearch,
-  faCheeseburger,
-  faCoffeeTogo,
-  faCode,
-  faEye,
-  faHandHoldingHeart,
-  fadUtensilsAlt,
-  falUtensilsAlt,
-  faLongArrowLeft,
   faMusic,
-  faTimes,
+  faSearch,
+  faBurgerCheese,
+  faXmark,
+  faEyes,
+  faCode,
+  faHandsHoldingHeart,
+  faCoffeePot,
+  faLongArrowLeft,
+  fatForkKnife,
+  fadForkKnife,
 );
 
 dom.watch();
